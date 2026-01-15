ff0846b3e3ee2310c8551ff57128bb00a60bbaac
rafael ibarra <rafaelibarra0795@gmail.com>
2026-01-15 19:01:28 -0300

    estructura de carpetas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LeadsModule } from './leads/leads.module';
+import { AuthModule } from './auth/auth.module';
+import { SyncModule } from './sync/sync.module';
+import { AiModule } from './ai/ai.module';
+import { QueueModule } from './queue/queue.module';
 
 @Module({
-  imports: [],
+  imports: [LeadsModule, AuthModule, SyncModule, AiModule, QueueModule],
   controllers: [AppController],
   providers: [AppService],
 })
