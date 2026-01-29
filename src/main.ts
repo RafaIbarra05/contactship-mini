d6042fd70aeee8d739152e87ef5d6383db630a3b
rafael ibarra <rafaelibarra0795@gmail.com>
2026-01-29 18:49:31 -0300

    chore: update app bootstrap configuration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 console.log(
   'DB URL:',
   (process.env.DATABASE_URL || '').replace(/:(\/\/.*?:)(.*?)(@)/, '$1***$3'),
@@ -7,6 +8,13 @@ console.log(
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
