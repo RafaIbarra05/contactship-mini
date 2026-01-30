763cadc778b357bdc221151725b072d952056e7f
rafael ibarra <rafaelibarra0795@gmail.com>
2026-01-30 19:21:44 -0300

    feat(leads): add basic leads endpoints

--- src/leads/leads.controller.ts
+++ src/leads/leads.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateLeadDto } from './dto/create-lead-dto';
+import { LeadsService } from './leads.service';
 
 @Controller('leads')
-export class LeadsController {}
+export class LeadsController {
+  constructor(private readonly leadsService: LeadsService) {}
+
+  @Post('create-lead')
+  createLeads(@Body() dto: CreateLeadDto) {
+    return this.leadsService.createLead(dto);
+  }
+
+  @Get('leads')
+  getLeads() {
+    return this.leadsService.getLeads();
+  }
+
+  @Get('lead/:id')
+  getLeadById(@Param('id') id: string) {
+    return this.leadsService.getLeadById(id);
+  }
+}
