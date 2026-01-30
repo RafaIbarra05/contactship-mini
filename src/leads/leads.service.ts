07e180b2c5aa439d64f9a9c0b056f91b99d52b08
rafael ibarra <rafaelibarra0795@gmail.com>
2026-01-30 19:24:16 -0300

    feat(leads): add leads service logic

--- src/leads/leads.service.ts
+++ src/leads/leads.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Lead } from './entities/lead.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateLeadDto } from './dto/create-lead-dto';
 
 @Injectable()
-export class LeadsService {}
+export class LeadsService {
+  constructor(
+    @InjectRepository(Lead)
+    private readonly LeadsRepo: Repository<Lead>,
+  ) {}
+
+  async createLead(dto: CreateLeadDto) {
+    const exists = await this.LeadsRepo.findOne({
+      where: { email: dto.email },
+    });
+    if (exists) throw new ConflictException('Lead already exist');
+
+    const Lead = this.LeadsRepo.create({
+      fullName: dto.fullName,
+      email: dto.email,
+      phone: dto.phone,
+      source: 'manual',
+    });
+    return this.LeadsRepo.save(Lead);
+  }
+  getLeads() {
+    return this.LeadsRepo.find({ order: { createdAt: 'DESC' } });
+  }
+  async getLeadById(id: string) {
+    const lead = await this.LeadsRepo.findOne({ where: { id } });
+    if (!lead) throw new NotFoundException('Lead not found');
+    return lead;
+  }
+}
