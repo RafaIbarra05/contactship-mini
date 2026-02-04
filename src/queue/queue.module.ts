e0c5c68db32f03649cc29b23958fab43c063b46d
rafael ibarra <rafaelibarra0795@gmail.com>
2026-02-04 20:18:11 -0300

    feat(queue): add bullmq queue module

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,4 +1,13 @@
+import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
+import { LeadsModule } from 'src/leads/leads.module';
 
-@Module({})
+@Module({
+  imports: [
+    BullModule.forRoot({ connection: { host: 'localhost', port: 6379 } }),
+    BullModule.registerQueue({ name: 'leads' }, LeadsModule),
+  ],
+  providers: [],
+  exports: [],
+})
 export class QueueModule {}
