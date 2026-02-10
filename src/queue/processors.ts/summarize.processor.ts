de9f32f118078aac2dfac328dea5ab42b2d7b54c
rafael ibarra <rafaelibarra0795@gmail.com>
2026-02-10 18:38:30 -0300

    feat(queue): add lead summarization processor

---
+++ src/queue/processors.ts/summarize.processor.ts
@@ -0,0 +1,42 @@
+import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { Inject } from '@nestjs/common';
+import { Job } from 'bullmq';
+import Redis from 'ioredis';
+import { LeadsService } from 'src/leads/leads.service';
+
+type SummarizedLeadJob = {
+  leadId: string;
+};
+@Processor('leads')
+export class SummarizeLeadProcessor extends WorkerHost {
+  private readonly Logger = new Logger(SummarizeLeadProcessor.name);
+  constructor(
+    private readonly leadsService: LeadsService,
+    private readonly iaService: AiService,
+    @Inject('REDIS_CLIENT')
+    private readonly redis: Redis,
+  ) {
+    super();
+  }
+
+  async process(job: Job<SummarizedLeadJob>): Promise<void> {
+    if (job.name !== 'summarize-lead') return;
+
+    const { leadId } = job.data;
+    this.Logger.log(`Processing summarize-lead for leadId=${leadId}`);
+
+    const lead = await this.leadsService.getLeadByIdNoCache(leadId);
+
+    const result = await this.iaService.summarizeLead({
+      fullName: lead.fullName,
+      email: lead.email,
+      phone: lead.phone,
+      source: lead.source,
+    });
+    await this.leadsService.saveLeadSummary(leadId, result);
+
+    await this.redis.del(`lead:${lead}`);
+
+    this.Logger.log(`Summarized completed for leadId:${leadId}`);
+  }
+}
