ffdc750bf87f9d4d548113e3748877841337138c
rafael ibarra <rafaelibarra0795@gmail.com>
2026-02-16 15:51:37 -0300

    fix(queue): adjust module configuration

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,13 +1,15 @@
 import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
 import { LeadsModule } from 'src/leads/leads.module';
+import { SummarizeLeadProcessor } from './processors.ts/summarize.processor';
 
 @Module({
   imports: [
     BullModule.forRoot({ connection: { host: 'localhost', port: 6379 } }),
-    BullModule.registerQueue({ name: 'leads' }, LeadsModule),
+    BullModule.registerQueue({ name: 'leads' }),
+    LeadsModule,
   ],
-  providers: [],
-  exports: [],
+  providers: [SummarizeLeadProcessor],
+  exports: [BullModule],
 })
 export class QueueModule {}
